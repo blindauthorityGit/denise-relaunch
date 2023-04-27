e1bab6c6d67f31335053eac52e8a1fe3c267e136
Johannes <office@atelierbuchner.at>
2023-04-27 10:18:56 +0200

    1366 test

--- components/buttons/mainButton.jsx
+++ components/buttons/mainButton.jsx
@@ -6,7 +6,7 @@ const MainButton = (props) => {
         <button
             type={props.type}
             onClick={props.onClick}
-            className="px-12 py-4 mt-8 min-w-[250px] max-w-[300px] w-full transition-all duration-500 hover:bg-primaryColor-600  text-lg sm:text-2xl uppercase tracking-widest text-white font-freight bg-primaryColor"
+            className="px-12 py-4 mt-8 min-w-[250px] max-w-[300px] w-full transition-all duration-500 hover:bg-primaryColor-600  text-lg md:text-xl xl:text-2xl uppercase tracking-widest text-white font-freight bg-primaryColor"
         >
             {props.children}
         </button>
