12567f3edbf942f4ca1971945ac06936b743d261
Johannes <office@atelierbuchner.at>
2023-04-27 13:40:22 +0200

    1366 test

--- components/imgText/bgText.jsx
+++ components/imgText/bgText.jsx
@@ -29,16 +29,16 @@ const BGText = (props) => {
         <div
             ref={ref}
             style={{ backgroundImage: `url(${urlFor(props.data.story.image).url()})` }}
-            className={`w-full container  sm:px-0 md:px-24 xl:px-48 m-auto bg-contain bg-no-repeat bg-center [background-position-x:400px] lg:gap-24 grid grid-cols-12  ${props.colspan}`}
+            className={`w-full container  sm:px-0 md:px-24 xl:px-48 m-auto bg-contain bg-no-repeat bg-center [background-position-x:400px] xl:gap-24 grid grid-cols-12  ${props.colspan}`}
         >
-            <div className="right px-8 py-8 lg:py-24 sm:px-0 col-span-12 lg:col-span-8 flex flex-col justify-center ">
+            <div className="right px-8 py-8 xl:py-24 sm:px-0 col-span-12 lg:col-span-8 flex flex-col justify-center ">
                 <h2
                     data-aos="fade-left"
                     className="font-freight text-2xl sm:text-4xl text-darkText xl:text-6xl font-thin mt-8 tracking-widest mb-8 lg:mb-12"
                 >
                     {props.data.story.title}
                 </h2>
-                <div className="font-freight text-lg">
+                <div className="font-freight xl:text-lg">
                     <PortableText value={props.data.story.text} />
                 </div>
             </div>
