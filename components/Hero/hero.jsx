ceef7e9e76a469c7bb99e3ea285e58aa6f66a10a
Johannes <office@atelierbuchner.at>
2023-05-08 17:58:37 +0200

    nu stuff

--- components/Hero/hero.jsx
+++ components/Hero/hero.jsx
@@ -33,7 +33,7 @@ const Hero = (props) => {
             <FloaterOpening data={props.dataSetting}></FloaterOpening>
             <FloaterContact data={props.dataSetting}></FloaterContact>
             <div className="text absolute z-10 text-center sm:w-[600px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <Parallax translateY={["-200px", "150px"]}>
+                <Parallax translateY={["-200px", "120px"]}>
                     <h1 className="text-white">{props.data.title}</h1>
                     <h3 className="mt-2 sm:mt-4 lg:mt-2 xl:mt-4 text-primaryColor-200">{props.data.subtitle}</h3>
                     <h6 className="mt-2 sm:mt-4 lg:mt-2 xl:mt-4 text-primaryColor-200">{props.data.subsubtitle}</h6>
